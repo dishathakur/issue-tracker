d59cd94bf3c1d04b1b6a0b696be5fa8c394ea27d
dishathakur <disha.thakur1994@gmail.com>
2025-03-07 17:13:05 +0530

    --removed unused variable

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "../client";
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   const issue = await prisma.issue.findMany();
   return NextResponse.json(issue);
 }
